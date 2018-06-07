d1723404d2bfa357747e57060abd8aa8f5f99c5e
saeedahmed78 <38943150+saeedahmed78@users.noreply.github.com>
2018-06-06 23:43:09 -0700

    one question remaining

---
+++ robot/script.js
@@ -0,0 +1,10 @@
+let question = "Salam", "Hi";
+
+let q1 = "";
+
+if (q1 == question) {
+	console.log("Thank you for calling TechKaro Customer Service. How may I help you?");
+}
+else{
+	console.log("nothing");
+}
---
+++ withdraw-program/script.js
@@ -0,0 +1,12 @@
+let totalAmount = 10000;
+let withDraw = 100;
+
+
+if (withDraw <= totalAmount) {
+
+console.log("SUCCESS! Your new balance is", totalAmount - withDraw);
+	
+}
+else{
+	console.log("FAILED! Sorry, you do not have sufficient balance.")
+}
