c947c908604c16e1055d23a723cc6671e899749d
saeedahmed78 <saeed01ahmed01@gmail.com>
2018-06-07 14:23:12 +0500

    completed

--- robot/script.js
+++ robot/script.js
@@ -1,10 +1,18 @@
-let question = "Salam", "Hi";
+let q1 = "Where will the class be conducted?";
 
-let q1 = "";
-
-if (q1 == question) {
+if (q1 == "Hi" || q1 == "Hello" || q1 == "Hey" || q1 == "Salam") {
 	console.log("Thank you for calling TechKaro Customer Service. How may I help you?");
 }
-else{
-	console.log("nothing");
+else if (q1 == "Is UIT is Open") {
+	console.log("Yes")
 }
+else if(q1 == "What are the timings of today's class?"){
+	console.log("1:30 - 5:30")
+}
+else if (q1 == "Where will the class be conducted?") {
+	console.log("	At CL-1, Usman Institute")
+}
+else{
+	console.log("Sorry, I do not understand your question.");
+}
+
---
+++ rocket/script.js
@@ -0,0 +1,11 @@
+let  raining = false;
+let snowing = false;
+let cloudy = false;
+let windSpeed = 11;
+if (raining == false && snowing == false && cloudy == false && windSpeed < 15 ) {
+	console.log("suitable for rocket launch");
+}
+else
+{
+	console.log("not suitable for rocket launch");
+}
